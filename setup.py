a29a14842829cb76d79620939ec55492b4b0a50c
Kelyn Crandall <kelyn@crandall.me>
2019-12-13 14:19:30 -0600

    Added Python 2.7 support and testing

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     ],
     version='0.1',
     packages=['qtrade_client', 'qtrade_client.cli'],
-    python_requires='>=3.5.0',
+    python_requires='>=2.7.0',
     entry_points={
         'console_scripts': ['qtapi = qtrade.cli:entry'],
     },
